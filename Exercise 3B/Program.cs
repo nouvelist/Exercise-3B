97ee999e93d39f0931fff7b7ac518e3fe5c88fe1
grace <grace@CHIAKI-LIV3S-HE>
2022-03-15 17:23:09 +1300

    Add project files.

---
+++ Exercise 3B/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter a number: ");
+
+int user;
+
+user = Convert.ToInt32(Console.ReadLine());
+
+int multiply = 5;
+
+int product;
+
+product = user * multiply;
+
+Console.WriteLine($"{user} x {multiply} = {product}");
